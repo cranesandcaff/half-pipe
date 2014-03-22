480f5f5dbb721b5294456ce37f949f7b82fa85da
Patrick Cauley <patrick@cranesandcaffeine.com>
2014-03-22 00:47:06 -0400

    Beginning addition of Gulp
    Gulp works better for my workflow, I am integrating that. This is the first time I've worked with modifying a gem, there are a few things I don't understand and could probably be done better. For instance inserting the stylesheet and application file into the view, I think I did that part wrong as I had to change which file was being referenced before it would work.
    
    Signed-off-by: Patrick Cauley <patrick@cranesandcaffeine.com>

--- lib/generators/half_pipe/install_generator.rb
+++ lib/generators/half_pipe/install_generator.rb
@@ -36,8 +36,8 @@ module HalfPipe
       end
 
       def insert_includes_into_layout
-        insert_into_file "app/views/layouts/application.html.erb", %Q{  <%= javascript_include_tag "/assets/scripts/application.js" %>\n  }, before: "</body>"
-        insert_into_file "app/views/layouts/application.html.erb", %Q{  <%= stylesheet_link_tag "/assets/styles/main" %>\n  }, before: "</head>"
+        insert_into_file "app/views/layouts/application.html.erb", %Q{  <%= javascript_include_tag "application.js" %>\n  }, before: "</body>"
+        insert_into_file "app/views/layouts/application.html.erb", %Q{  <%= stylesheet_link_tag "main" %>\n  }, before: "</head>"
       end
 
       def generate_scripts
